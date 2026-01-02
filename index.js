c552b742bb619516d26897c0cf4fbf1f45d4c50f
Jaskaran Singh <jsingh21002@gmail.com>
2026-01-01 22:47:37 -0500

    basic functionality added

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
+
+app.post("/submit", (req, res) => {
+  const title = req.body.Title;
+  const content = req.body.Content;
+  console.log(`Title: ${title}, Content: ${content}`);
+  res.send("Form submitted successfully!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
